6347db0c1cc60800c530347dc465ff81f0062e57
Victor-Spichenkoff <victorspich13@gmail.com>
2025-10-01 18:24:42 -0300

    Add auto

--- src/controllers/redirect.ts
+++ src/controllers/redirect.ts
@@ -41,22 +41,22 @@ export const redirectController = async (req: Request, res: Response) => {
     //         }).catch(console.error);
     //     });
 
-    //     (async() => {
-    //         await parallelProcess({
-    //             header,
-    //             ip: ip ?? "NONE",
-    //             projectName,
-    //             extra
-    //         }).catch(console.error);
-    //     })()
-
-
-     await parallelProcess({
+        (async() => {
+            await parallelProcess({
                 header,
                 ip: ip ?? "NONE",
                 projectName,
                 extra
-            })
+            }).catch(console.error);
+        })()
+
+
+    //  await parallelProcess({
+    //             header,
+    //             ip: ip ?? "NONE",
+    //             projectName,
+    //             extra
+    //         })
 
     if (process.env.NO_REDIRECT == "true")
         return res.send(dest)
